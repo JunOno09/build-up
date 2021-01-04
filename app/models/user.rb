c8d7070b63595338ed70a300d51c06cc837f0c8f
JunOno09 <jastis.09@gmail.com>
2021-01-04 14:53:24 +0900

    rspec、FactroyBotの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :email, presence: true,uniqueness: true
+
+         with_options presence: true do
+          validates :nickname
+          validates :birth_day
+         end
+
+         validates :password, presence: true,length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+          validates :family_name
+          validates :first_name
+         end
+
+         with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do 
+          validates :family_name_kana
+          validates :first_name_kana
+         end
 end
