ac1df5c5f2c984bd55c2ee6631c2a46121114835
JunOno09 <jastis.09@gmail.com>
2021-01-13 16:38:30 +0900

    モデルの生成、マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_022822) do
+ActiveRecord::Schema.define(version: 2021_01_13_073635) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2021_01_11_022822) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_id"], name: "index_comments_on_record_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "record_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_id"], name: "index_likes_on_record_id"
+    t.index ["user_id", "record_id"], name: "index_likes_on_user_id_and_record_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -79,5 +89,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_022822) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "records"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "records"
+  add_foreign_key "likes", "users"
   add_foreign_key "records", "users"
 end
