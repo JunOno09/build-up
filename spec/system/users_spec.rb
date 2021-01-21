78c8cb883c96ea74286a49fb182acf3477efb5ff
JunOno09 <jastis.09@gmail.com>
2021-01-21 11:50:03 +0900

    登録機能の結合テスト

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+def visit_with_http_auth(path)
+  admin = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{admin}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      visit_with_http_auth root_path
+      # トップページに移動する
+      #visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user[nickname]', with: @user.nickname
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
+      fill_in 'user[password_confirmation]', with: @user.password_confirmation 
+      fill_in 'user[family_name]', with: @user.family_name
+      fill_in 'user[first_name]', with: @user.first_name
+      fill_in 'user[family_name_kana]', with: @user.family_name_kana
+      fill_in 'user[first_name_kana]', with: @user.first_name_kana
+      select '1930', from: 'user_birth_day_1i'
+      select '1', from: 'user_birth_day_2i'
+      select '19', from: 'user_birth_day_3i'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq root_path
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit_with_http_auth root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user[nickname]', with: ""
+      fill_in 'user[email]', with: ""
+      fill_in 'user[password]', with: ""
+      fill_in 'user[password_confirmation]', with: ""
+      fill_in 'user[family_name]', with: ""
+      fill_in 'user[first_name]', with: ""
+      fill_in 'user[family_name_kana]', with: ""
+      fill_in 'user[first_name_kana]', with: ""
+      select "--", from: 'user_birth_day_1i'
+      select "--", from: 'user_birth_day_2i'
+      select "--", from: 'user_birth_day_3i'
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
+end
